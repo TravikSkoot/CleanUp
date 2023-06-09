3b471a4788b512aadd825a73300d191a38d299b6
TravikSkoot <67009618+TravikSkoot@users.noreply.github.com>
2023-06-10 00:30:07 +0200

    Update cleanup.py

--- cleanup.py
+++ cleanup.py
@@ -1,17 +1,19 @@
 import os
 import shutil
 
-# Definiere den aktuellen Ordner, in dem sich das Skript befindet, als Downloads-Ordner
+# Definiere den aktuellen Ordner, in dem sich das Skript befindet
 current_folder = os.path.dirname(os.path.abspath(__file__))
 
-# Erstelle eine Liste aller Dateien und Ordner im Downloads-Ordner
+# Erstelle eine Liste aller Dateien und Ordner im aktuellen Ordner
 folder_contents = os.listdir(current_folder)
 
-# Dateiendungen
+# ---------------------------------------------------------------------------
+# Hier kannst du Dateiendungen anpassen
 picture_extensions = ".png", ".jpg", ".jpeg", ".gif", ".bmp", ".svg", ".ico"
 video_extensions = ".mp4", ".mkv", ".avi", ".mov", ".wmv"
 document_extensions = ".doc", ".docx", ".pdf", ".xlsx", ".pptx", ".txt"
-arhcive_extensions = ".zip", ".tar", ".tar.gz", ".gz", ".rar"
+archive_extensions = ".zip", ".tar", ".tar.gz", ".gz", ".rar"
+# ---------------------------------------------------------------------------
 
 # Erstelle die Ordner für die verschiedenen Kategorien, falls mindestens eine entsprechende Datei gefunden wurde
 picture_folder = os.path.join(current_folder, "Bilder")
@@ -30,7 +32,7 @@ if any(item.lower().endswith(document_extensions) for item in folder_contents):
         os.mkdir(document_folder)
 
 archive_folder = os.path.join(current_folder, "Archive")
-if any(item.lower().endswith(arhcive_extensions) for item in folder_contents):
+if any(item.lower().endswith(archive_extensions) for item in folder_contents):
     if not os.path.exists(archive_folder):
         os.mkdir(archive_folder)
 
@@ -44,7 +46,7 @@ if any(not os.path.isdir(os.path.join(current_folder, item)) and not os.path.isl
     if not os.path.exists(other_folder):
         os.mkdir(other_folder)
 
-# Gehe durch alle Dateien und Ordner im Downloads-Ordner
+# Gehe durch alle Dateien und Ordner
 for item in folder_contents:
 
     item_path = os.path.join(current_folder, item)
@@ -53,7 +55,7 @@ for item in folder_contents:
     if item == os.path.basename(__file__):
         continue
     
-    # Überspringe Verknüpfungen
+    # TODO Überspringe Verknüpfungen
     if os.path.islink(item_path):
         continue
 
@@ -71,7 +73,7 @@ for item in folder_contents:
     elif item.lower().endswith((document_extensions)):
         shutil.move(item_path, os.path.join(document_folder, item))
         
-    elif item.lower().endswith((arhcive_extensions)):
+    elif item.lower().endswith((archive_extensions)):
         shutil.move(item_path, os.path.join(archive_folder, item))
         
     elif os.path.isdir(item_path):
